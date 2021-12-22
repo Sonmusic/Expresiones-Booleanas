b1b877a1fc4a8b02ff58222b601d1532e985ad30
Sonmusic <64028850+Sonmusic@users.noreply.github.com>
2021-12-22 18:51:11 -0400

    Tests Not Equal + Test Exception

--- TestExpresionesBooleanas/UnitTest1.cs
+++ TestExpresionesBooleanas/UnitTest1.cs
@@ -6,24 +6,55 @@ namespace TestExpresionesBooleanas
     public class UnitTest1
     {
         [TestMethod]
-        public void Return_True_Tabla_1_Variable()
+        public void Return_Equal_Tabla_1_Variable()
         {
             ExpresionesBooleanas.Expresion expresion = new ExpresionesBooleanas.Expresion("*A");
             Assert.AreEqual("|1|\n|0|", expresion.TablaBooleana());
         }
         [TestMethod]
-        public void Return_True_Tabla_2_Variables()
+        public void Return_Equal_Tabla_2_Variables()
         {
             ExpresionesBooleanas.Expresion expresion = new ExpresionesBooleanas.Expresion("(A || B) && (*A && B)");
             Assert.AreEqual("|1| |1|\n|1| |0|\n|0| |1|\n|0| |0|", expresion.TablaBooleana());
         }
         [TestMethod]
-        public void Return_True_Tabla_3_Variables()
+        public void Return_NotEqual_Tabla_3_Variables()
         {
             ExpresionesBooleanas.Expresion expresion = new ExpresionesBooleanas.Expresion("((A || B) && (*A && B)) || C");
             Assert.AreEqual("|1| |1| |1|\n|1| |1| |0|\n|1| |0| |1|\n|1| |0| |0|\n|0| |1| |1|\n|0| |1| |0|\n|0| |0| |1|\n|0| |0| |0|\n"
                 , expresion.TablaBooleana());
         }
+        [TestMethod]
+        public void Return_NotEqual_Tabla_1_Variables()
+        {
+            ExpresionesBooleanas.Expresion expresion = new ExpresionesBooleanas.Expresion("A");
+            Assert.AreNotEqual("|0|\n|0|", expresion.TablaBooleana());
+        }
+        [TestMethod]
+        public void Return_NotEqual_Tabla_2_Variables()
+        {
+            ExpresionesBooleanas.Expresion expresion = new ExpresionesBooleanas.Expresion("(A || B) && (*A && B)");
+            Assert.AreNotEqual("|1| |1|\n|1| |0|\n|0| |1|\n|1| |0|", expresion.TablaBooleana());
+        }
+        [TestMethod]
+        public void Return_Equal_Tabla_3_Variables()
+        {
+            ExpresionesBooleanas.Expresion expresion = new ExpresionesBooleanas.Expresion("((A || B) && (*A && B)) || C");
+            Assert.AreNotEqual("|0| |1| |1|\n|1| |1| |0|\n|1| |0| |1|\n|1| |0| |0|\n|0| |1| |1|\n|0| |1| |0|\n|0| |0| |1|\n|0| |0| |0|\n"
+                , expresion.TablaBooleana());
+        }
+        [TestMethod]
+        public void Return_Error_Null_Tabla()
+        {
+            try
+            {
+                ExpresionesBooleanas.Expresion expresion = new ExpresionesBooleanas.Expresion("");
+            }
+            catch (System.Exception)
+            {
 
+                Assert.IsTrue(true);
+            }
+        }
     }
 }
